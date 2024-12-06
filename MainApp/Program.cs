e80de2a2e84c3c691ae6ec3221c926b80564fd08
Noseh Taghaymurodzoda <noseh.tagaymurodzoda@gmail.com>
2024-12-06 17:18:46 +0500

    fixbug

--- MainApp/Program.cs
+++ MainApp/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Model.Courses;
+using Model.Students;
+using Model.Teachers;
+using Infrastructure.Service;
+
+
+Teacher teachers = new Teacher();
+TeacherService teacherService = new TeacherService();
+var teacher = teacherService.GetTaechers();
+foreach (var t in teacher)
+{
+    Console.WriteLine(t.Id + " || " + t.FullName+ " || " + t.Email + " || " + t.Phone + " || " + t.Address);
+}
